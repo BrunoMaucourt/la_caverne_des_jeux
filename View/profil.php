c4490e508774731f3723f696aab3e95c7838ee95
Bruno <mct.bruno@gmail.com>
2023-01-25 17:18:58 +0100

    disconnect script

--- View/profil.php
+++ View/profil.php
@@ -1,100 +1,127 @@
 <?php
     session_start();
     $title = "Mon profil";
+    define("ADMIN_LEVEL", 1);
+    define("SELLER_LEVEL", 2);
     require_once "../Controller/check_user_connected.php";
     require_once "../Element/header.php";
 ?>
 <h1>Mon profil</h1>
 
-<div class="container mx-auto row-cols-1 row-cols-lg-2">
-    <div class="row text-center">
-        <div class="col p-3">
-            <div class="card text-bg-info">
-                <a href="" class="text-decoration-none">
-                <div class="card-header">Mes informations</div>
-                    <div class="card-body">
-                        <h5 class="card-title text-decoration-none">Info card title</h5>
-                        <p class="card-text text-decoration-none">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<div class="container">
+    <div class="row mx-auto row-cols-1 row-cols-lg-2 text-center">
+    <?PHP
+            if($_SESSION['account_level_id'] == ADMIN_LEVEL){
+                echo <<<HTML
+                <div class="col p-3">
+                    <div class="card text-bg-light">
+                        <a href="" class="text-decoration-none">
+                        <div class="card-header">Ajouter une catégorie de jeux</div>
+                        <div class="card-body">
+                        <p class="card-text">Ajouter une nouvelle catégorie de jeux</p>
+                        </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-        </div>
-        <div class="col p-3">
-            <div class="card text-bg-danger">
-                <a href="" class="text-decoration-none">
-                <div class="card-header">Se déconnecter</div>
-                    <div class="card-body">
-                        <h5 class="card-title">Info card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <div class="col p-3">
+                    <div class="card text-bg-light">
+                        <a href="" class="text-decoration-none">
+                        <div class="card-header">Ajouter une tranche d'âge</div>
+                        <div class="card-body">
+                        <p class="card-text">Ajouter une nouvelle tranche d'âge</p>
+                        </div>
+                        </a>
                     </div>
-                </a>
-            </div>
-        </div>
-        <?PHP
-            if($_SESSION['account_level_id'] == 1){
-                // admin
-                echo '<div class="col p-3">';
-                echo '<div class="card text-bg-danger">';
-                echo '<a href="" class="text-decoration-none">';
-                echo '<div class="card-header">Se déconnecter</div>';
-                echo '<div class="card-body">';
-                echo '<h5 class="card-title">Info card title</h5>';
-                echo '<p class="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>';
-                echo '</div>';
-                echo '</a>';
-                echo '</div>';
-                echo '</div>';
-
-                // Ajouter tranche age
-                // AJouter éditeur
-                // AJouter nombre joueur
-                // Modifier utilisateur
-                // Modifier status utilisateur
-            }elseif($_SESSION['account_level_id'] == 2){
+                </div>
+                <div class="col p-3">
+                    <div class="card text-bg-light">
+                        <a href="" class="text-decoration-none">
+                        <div class="card-header">Ajouter un éditeur</div>
+                        <div class="card-body">
+                        <p class="card-text">Ajouter un nouvel utilisateur</p>
+                        </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col p-3">
+                    <div class="card text-bg-light">
+                        <a href="" class="text-decoration-none">
+                        <div class="card-header">Ajouter un nombre de joueurs</div>
+                        <div class="card-body">
+                        <p class="card-text">Ajouter un nouveau nombre de joueurs</p>
+                        </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col p-3">
+                    <div class="card text-bg-light">
+                        <a href="" class="text-decoration-none">
+                        <div class="card-header">Modifier utilisateur</div>
+                        <div class="card-body">
+                        <p class="card-text">Modifier les informations d'un utilisateur</p>
+                        </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col p-3">
+                    <div class="card text-bg-light">
+                        <a href="" class="text-decoration-none">
+                        <div class="card-header">Modifier le status d'un utilisateur</div>
+                        <div class="card-body">
+                        <p class="card-text">Accorder un nouveau status à un utilisateur</p>
+                        </div>
+                        </a>
+                    </div>
+                </div>
+HTML;
+            }elseif($_SESSION['account_level_id'] == SELLER_LEVEL){
                 // vendeur
                 echo <<<HTML
                 <div class="col p-3">
-                    <div class="card text-bg-danger">
-                        <a href="" class="text-decoration-none">
-                        <div class="card-header">Ajouter jeu</div>
+                    <div class="card text-bg-light">
+                        <a href="./add_game.php" class="text-decoration-none">
+                        <div class="card-header">Ajouter un jeu</div>
                         <div class="card-body">
-                        <h5 class="card-title">Info card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cardcontent.</p>
+                        <p class="card-text">Ajouter un nouveau jeu</p>
+                        </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col p-3">
+                    <div class="card text-bg-light">
+                        <a href="./add_game.php" class="text-decoration-none">
+                        <div class="card-header">Modifier un jeu</div>
+                        <div class="card-body">
+                        <p class="card-text">Modifier les informations d'un jeu ou le supprimer de la liste</p>
                         </div>
                         </a>
                     </div>
                 </div>
 HTML;
             }
-            ?>
+        ?>
+        <div class="col p-3">
+            <div class="card text-bg-light">
+                <a href="./settings.php" class="text-decoration-none">
+                <div class="card-header">Mes informations</div>
+                    <div class="card-body">
+                        <p class="card-text text-decoration-none">Modifier les informations de mon profil</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="col p-3">
+            <div class="card text-bg-danger">
+                <a href="../Controller/user_disconnect.php" class="text-decoration-none">
+                <div class="card-header">Se déconnecter</div>
+                    <div class="card-body">
+                        <p class="card-text">Vous allez nous manquer</p>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
-
-<?PHP
-if($_SESSION['account_level_id'] == 1){
-    // admin
-    // AJouter catégorie
-    // Ajouter tranche age
-    // AJouter éditeur
-    // AJouter nombre joueur
-    // Modifier utilisateur
-    // Modifier status utilisateur
-}elseif($_SESSION['account_level_id'] == 2){
-    // vendeur
-    // ajouter un jeu
-}else{
-    // Client
-    // Mon Panier
-}
-
-    // Mes informations
-    // Se déconnecter
-
-
-?>
-
-
-
 
 <?php
     require_once "../Element/footer.php";
