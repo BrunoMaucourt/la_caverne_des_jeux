803fe36704c8b440b9d188ac54f1e8bff902967b
Thibault Couanon <couanonthibault@hotmail.fr>
2023-01-26 19:30:04 +0100

    dynamic and saved user cart, for both client or not connected users

--- Controller/modify_cart.php
+++ Controller/modify_cart.php
@@ -2,31 +2,46 @@
 session_start();
 require_once "db_connection.php";
 
+//if the user is not a client
 if(!isset($_SESSION['user_id'])){
-    if(!isset($_SESSION['non_registered_user_cart'])){
-        echo "pas de panier";
-        //$non_registered_user_cart = array();
-        //$non_registered_user_cart = [$product_id => 1];
-        //$_SESSION['non_registered_user_cart'] = $non_registered_user_cart;
-    }else{
-        echo "un panier";
-        //$non_registered_user_cart = $_SESSION['non_registered_user_cart'];
-        //if(array_key_exists($product_id,$non_registered_user_cart)){
-          //  $non_registered_user_cart[$product_id] += 1;
-        //}else{
-         //   $non_registered_user_cart = [$product_id => 1];
-        //}
-        //$_SESSION['non_registered_user_cart'] = $non_registered_user_cart;
-        //var_dump($_SESSION['non_registered_user_cart']);
-    }   
+    //if the button pressed is the less-btn
+    if($_POST['quantity_btn'] == 'less'){
+        //we loop inside the non registered user cart to minus one the 
+        //value of the quantity of the product_id
+        foreach($_SESSION['non_registered_user_cart'] as $key => $value) {
+            if($key == $_POST['product_id']){
+                $value--;
+                if($value == 0){
+                    unset($_SESSION['non_registered_user_cart'][$key]);
+                }else{
+                    $_SESSION['non_registered_user_cart'][$key] = $value;
+                } 
+            }
+        }
+        //if the non registered user cart is empty, we delete it
+        if(count($_SESSION['non_registered_user_cart']) == 0){
+            unset($_SESSION['non_registered_user_cart']);
+        } 
+        header("Location: ../View/cart.php");
+        exit();
+    //if the button pressed is plus button    
+    }elseif($_POST['quantity_btn']== 'plus'){
+        $_SESSION['non_registered_user_cart'][$_POST['product_id']]++;
+        header("Location: ../View/cart.php");
+        exit();
+    }
+//if the user is a registered client
 }else{
     $product_id = $_POST['product_id'];
+    //check if the user is registered as a client
     if($_SESSION['account_level_id'] == 3){
+        //if btn pressed is minus button
         if($_POST['quantity_btn']== 'less'){
             $query = "DELETE FROM `user_cart` WHERE game_id = $product_id LIMIT 1";
             $stmt = $dbh_readwrite->query($query);
             header("Location: ../View/cart.php");
             exit();
+        //if btn pressed is plus button
         }elseif($_POST['quantity_btn']== 'plus'){
             $query = "INSERT INTO `user_cart`(`user_id`, `game_id`) VALUES (?,$product_id)";
             $stmt = $dbh_readwrite->prepare($query);
@@ -34,7 +49,7 @@ if(!isset($_SESSION['user_id'])){
             header("Location: ../View/cart.php");
             exit();
         }
-        
+    //if the use is not a client (admin or seller)
     }else{
         header("Location: ../View/index.php");
         exit();
--- View/cart.php
+++ View/cart.php
@@ -1,21 +1,74 @@
 <?php
     session_start();
     $title = "Mon panier";
+
+    //var to display or not user cart
+    $display_cart = false;
+
+    //if the user is not connected
     if(!isset($_SESSION['user_id'])){
         require_once "../Element/header.php";
         
+        //check if a session cart exist
+        //if not we set the display to false
+        if(!isset($_SESSION['non_registered_user_cart'])){
+            $display_cart = false;
+
+        //else we set the display to true
+        //with the SESSION card, making a array with same keys as a SQLquery array
+        }else{
+            $display_cart = true;
+            $non_registered_user_cart = $_SESSION['non_registered_user_cart'];
+            $temp_cart = array();
+            $cart = array();
+            $index = 0;
+            foreach ($non_registered_user_cart as $key => $game) {
+                $query = "SELECT name, price, game_picture, description FROM game WHERE game.id = $key";
+                $stmt = $dbh_readonly->query($query);
+                $cart[$index] = $stmt->fetch(PDO::FETCH_ASSOC);
+                $cart[$index]['quantity'] = $game;
+                $cart[$index]['game_id'] = $key;
+                $index++;
+            }            
+        } 
+    //if user is connected               
     }else{
         require_once "../Controller/check_is_client.php";
         require_once "../Element/header.php";
 
-        $query = "SELECT user_id,UC.game_id, COUNT(*) quantity, G.name, G.price, G.game_picture, G.description FROM `user_cart` UC INNER JOIN game G ON G.id = UC.game_id WHERE UC.user_id = ? GROUP BY game_id";
+        //first sql query to get all the product the user cart DB
+        $query = "SELECT COUNT(*) number FROM user_cart WHERE user_id = ?";
         $stmt = $dbh_readonly->prepare($query);
         $stmt->execute([$_SESSION['user_id']]);
-        $cart = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $cart = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        //if there is no product in the array we set the display to false
+        if($cart['number'] == 0){
+            $display_cart = false;
 
-        echo "<h1>Mon panier</h1>";
+        //if there is at least one product,we set the display to true
+        //then we make an assoc array wi the different product ant their quantities
+        }else{
+            $display_cart = true;
+            $query = "SELECT user_id,UC.game_id, COUNT(*) quantity, G.name, G.price, G.game_picture, G.description FROM `user_cart` UC INNER JOIN game G ON G.id = UC.game_id WHERE UC.user_id = ? GROUP BY game_id";
+            $stmt = $dbh_readonly->prepare($query);
+            $stmt->execute([$_SESSION['user_id']]);
+            $cart = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }    
+        
+    
+    //at this point, in both case (connected or not) we have an array named $cart
+    //we generate a dynamic html with it.
+    echo "<h1>Mon panier</h1>";
 
+    if(!$display_cart){
+        echo '<div class="row row-cols-4 m-5 text-center">
+        <div class="col-4 m-auto border">
+            <h3> Votre Panier est vide </h3>
+        </div>
+    </div>';
+    }else{
         foreach ($cart as $product) {
             echo '<div class="row row-cols-12 m-3 border">
                     <div class="col-2 border d-flex justify-content-center align-items-center">
@@ -50,37 +103,35 @@
                     </div>  
                  </div>';
         }
-    }
-    
-?>
-
-
-<?php
-    
-?>
-<div class="row row-cols-3 m-3 text-center">
-    <div class="col-auto m-auto border">
-        <div class="row">
-            <h3>PRIX TOTAL DE VOTRE PANIER</h3>
-        </div>
-        <div class="row">
-            <h3><?php  
-                $total = 0;
-                foreach ($cart as $product) {
-                    $total += $product['price']*$product['quantity'];
-                }
-                echo number_format($total,2)."€";
-            ?></h3>
-        </div>
+        echo '<div class="row row-cols-3 m-3 text-center">
+            <div class="col-auto m-auto border">
+                <div class="row">
+                    <h3>PRIX TOTAL DE VOTRE PANIER</h3>
+                </div>
+                <div class="row">
+                    <h3>';
+                        $total = 0;
+                        foreach ($cart as $product) {
+                            $total += $product['price']*$product['quantity'];
+                        }
+                        echo number_format($total,2)."€".'
+                    </h3>
+                </div>
+                
         
-
-    </div>       
-</div>
-<div class="row row-cols-12 m-auto">
+            </div>       
+        </div>
+        <div class="row row-cols-12 m-auto">
     <div class="col-2 m-auto">
         <button type="button" class="btn btn-success">Payer votre panier</button>
     </div>                      
-</div>
+</div>';
+    }    
+?>
+
+
+
+
 <?php
     require_once "../Element/footer.php";
 ?>
