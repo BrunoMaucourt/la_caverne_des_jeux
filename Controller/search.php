d0305dc5e0c25fafd6ad87f8ab363c1abffb3904
Thibault Couanon <couanonthibault@hotmail.fr>
2023-01-27 11:11:38 +0100

    fix for empty search input error

--- Controller/search.php
+++ Controller/search.php
@@ -4,14 +4,18 @@
     $search = $_GET["search"];
 
     //space search input are not allowed. send back to last visited page.
-    if($search =="" || $search == " " || $search == "  " || $search == "   "){
+    if($search == "" || $search == " " || $search == "  " || $search == "   "){
         header('Location: ' . $_SERVER['HTTP_REFERER']);
+        exit();
+        
     //if the input is correct
     }else{
         $query = "SELECT * FROM `game` WHERE name LIKE '%$search%' OR description LIKE '%$search%' ";
         $stmt = $dbh_readonly->query($query);
         $games = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $_SESSION['searched_games'] = $games;
+        header('Location: ../View/display_search.php');
+        exit();
     }
-    $_SESSION['searched_games'] = $games;
-    header('Location: ../View/display_search.php');
+    
 ?>
